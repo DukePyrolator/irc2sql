d7832197378182992cfaf3a7d99d07aea08d93af
DukePyrolator <DukePyrolator@anope.org>
2013-06-02 14:26:52 +0200

    cleanup, fixed reading the config, fixed prefixes

--- irc2sql.cpp
+++ irc2sql.cpp
@@ -1,11 +1,6 @@
 #include "module.h"
 #include "modules/sql.h"
 
-struct ExtensibleString : Anope::string, ExtensibleItem
-{
-	ExtensibleString(const Anope::string &s) : Anope::string(s) { }
-};
-
 class MySQLInterface : public SQL::Interface
 {
  public:
@@ -18,9 +13,9 @@ class MySQLInterface : public SQL::Interface
 	void OnError(const SQL::Result &r) anope_override
 	{
 		if (!r.GetQuery().query.empty())
-			Log(LOG_DEBUG) << "m_stats: Error executing query " << r.finished_query << ": " << r.GetError();
+			Log(LOG_DEBUG) << "m_irc2sql: Error executing query " << r.finished_query << ": " << r.GetError();
 		else
-			Log(LOG_DEBUG) << "m_stats: Error executing query: " << r.GetError();
+			Log(LOG_DEBUG) << "m_irc2sql: Error executing query: " << r.GetError();
 	}
 };
 
@@ -29,7 +24,7 @@ class M_irc2sql : public Module
 	ServiceReference<SQL::Provider> sql;
 	MySQLInterface sqlinterface;
 	SQL::Query query;
-	std::vector<Anope::string> TableList;
+	std::vector<Anope::string> TableList, ProcedureList, EventList;
 	Anope::string prefix;
 
 	void RunQuery(const SQL::Query &q)
@@ -41,6 +36,8 @@ class M_irc2sql : public Module
 	void GetTables()
 	{
 		TableList.clear();
+		ProcedureList.clear();
+		EventList.clear();
 		if (!sql)
 			return;
 
@@ -51,6 +48,18 @@ class M_irc2sql : public Module
 			for (std::map<Anope::string, Anope::string>::const_iterator it = map.begin(); it != map.end(); ++it)
 				TableList.push_back(it->second);
 		}
+		query = "SHOW PROCEDURE STATUS WHERE `Db` = Database();";
+		r = this->sql->RunQuery(query);
+		for (int i = 0; i < r.Rows(); ++i)
+		{
+			ProcedureList.push_back(r.Get(i, "Name"));
+		}
+		query = "SHOW EVENTS WHERE `Db` = Database();";
+		r = this->sql->RunQuery(query);
+		for (int i = 0; i < r.Rows(); ++i)
+		{
+			EventList.push_back(r.Get(i, "Name"));
+		}
 	}
 
 	bool HasTable(const Anope::string &table)
@@ -60,6 +69,23 @@ class M_irc2sql : public Module
 				return true;
 		return false;
 	}
+
+	bool HasProcedure(const Anope::string &table)
+	{
+		for (std::vector<Anope::string>::const_iterator it = ProcedureList.begin(); it != ProcedureList.end(); ++it)
+			if (*it == table)
+				return true;
+		return false;
+	}
+
+	bool HasEvent(const Anope::string &table)
+	{
+		for (std::vector<Anope::string>::const_iterator it = EventList.begin(); it != EventList.end(); ++it)
+			if (*it == table)
+				return true;
+		return false;
+	}
+
 
 	void CheckTables()
 	{
@@ -138,33 +164,26 @@ class M_irc2sql : public Module
 	M_irc2sql(const Anope::string &modname, const Anope::string &creator) :
 		Module(modname, creator, EXTRA | THIRD), sql("", ""), sqlinterface(this)
 	{
-		this->SetAuthor("Anope");
-	}
-
-	~M_irc2sql()
-	{
-		// destructor
 	}
 
 	void OnReload(Configuration::Conf *config) anope_override
 	{
 		Configuration::Block *block = config->GetModule(this);
 
-		prefix = block->Get<const Anope::string>("prefix", "anope_stats_");
-
+		prefix = block->Get<const Anope::string>("prefix", "anope_");
 		Anope::string engine = block->Get<const Anope::string>("engine");
-		this->sql = ServiceReference<SQL::Provider>("SQLProvider", engine);
+		this->sql = ServiceReference<SQL::Provider>("SQL::Provider", engine);
 		if (sql)
 			this->CheckTables();
-		else
-			Log() << "irc2sql: no database connection to " << engine;
+			else
+			Log() << "m_irc2sql: no database connection to " << engine;
 	}
 
 	void OnNewServer(Server *server) anope_override
 	{
-		query = "INSERT DELAYED INTO `anope_stats_servers` (name, hops, comment, link_time, online, ulined) "
+		query = "INSERT DELAYED INTO `" + prefix + "servers` (name, hops, comment, link_time, online, ulined) "
 			"VALUES (@name@, @hops@, @comment@, unix_timestamp(), '1', @ulined@) "
-			"ON DUPLICATE KEY UPDATE name=VALUES(name), hops=VALUES(hops), comment=VALUES(comment), link_time=VALUES(link_time), online=VALUES(online), ulined=(ulined);";
+			"ON DUPLICATE KEY UPDATE name=VALUES(name), hops=VALUES(hops), comment=VALUES(comment), link_time=VALUES(link_time), online=VALUES(online), ulined=(ulined)";
 		query.SetValue("name", server->GetName());
 		query.SetValue("hops", server->GetHops());
 		query.SetValue("comment", server->GetDescription());
@@ -174,9 +193,9 @@ class M_irc2sql : public Module
 
 	void OnServerQuit(Server *server) anope_override
 	{
-		query = "INSERT DELAYED INTO `anope_stats_servers` (name, online, split_time)"
+		query = "INSERT DELAYED INTO `" + prefix + "servers` (name, online, split_time) "
 			"VALUES (@name@, '0', unix_timestamp()) "
-			"ON DUPLICATE KEY UPDATE name=VALUES(name), online=VALUES(online), split_time=VALUES(split_time);";
+			"ON DUPLICATE KEY UPDATE name=VALUES(name), online=VALUES(online), split_time=VALUES(split_time)";
 		query.SetValue("name", server->GetName());
 		this->RunQuery(query);
 	}

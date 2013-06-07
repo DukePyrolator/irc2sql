29aa72323316a28d5b0f6703ae3e421b89049644
DukePyrolator <DukePyrolator@anope.org>
2013-06-07 06:57:52 +0200

    more work done

--- irc2sql.cpp
+++ irc2sql.cpp
@@ -26,6 +26,7 @@ class M_irc2sql : public Module
 	SQL::Query query;
 	std::vector<Anope::string> TableList, ProcedureList, EventList;
 	Anope::string prefix;
+	bool quitting;
 
 	void RunQuery(const SQL::Query &q)
 	{
@@ -113,7 +114,7 @@ class M_irc2sql : public Module
 		if (!this->HasTable(prefix + "countries"))
 		{
 			query = "CREATE TABLE `" + prefix + "countries` ("
-				"`id` int(11) NOT NULL AUTO_INCREMENT,"
+				"`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,"
 				"`code` varchar(3) NOT NULL,"
 				"`name` varchar(32) NOT NULL,"
 				"`count` int(11) NOT NULL,"
@@ -127,7 +128,7 @@ class M_irc2sql : public Module
 		if (!this->HasTable(prefix + "servers"))
 		{
 			query = "CREATE TABLE `" + prefix + "servers` ("
-				"`id` int(11) NOT NULL AUTO_INCREMENT,"
+				"`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,"
 				"`name` varchar(64) NOT NULL,"
 				"`hops` tinyint(3) NOT NULL,"
 				"`comment` varchar(255) NOT NULL,"
@@ -136,24 +137,68 @@ class M_irc2sql : public Module
 				"`online` tinyint(1) NOT NULL DEFAULT '1',"
 				"`split_time` int(11) DEFAULT NULL,"
 				"`version` varchar(127) NOT NULL,"
-				"`uptime` mediumint(9) NOT NULL,"
+				"`uptime` mediumint(9) NOT NULL,"         // remove or keep?
 				"`motd` text NOT NULL,"
-				"`users_current` smallint(6) NOT NULL,"
-				"`users_max` smallint(6) NOT NULL,"
-				"`users_max_time` int(11) NOT NULL,"
-				"`ping` smallint(6) NOT NULL,"
-				"`ping_max` smallint(6) NOT NULL,"
-				"`ping_time` int(11) NOT NULL,"
-				"`ping_max_time` int(11) NOT NULL,"
+				"`users_current` smallint(6) NOT NULL,"   // remove or use triggers to fill it?
+				"`users_max` smallint(6) NOT NULL,"       // remove or use triggers to fill it?
+				"`users_max_time` int(11) NOT NULL,"      // remove this field ?
+				"`ping` smallint(6) NOT NULL,"            // remove this field ?
+				"`ping_max` smallint(6) NOT NULL,"        // remove this field ?
+				"`ping_time` int(11) NOT NULL,"           // remove this field ?
+				"`ping_max_time` int(11) NOT NULL,"       // remove this field ?
 				"`ulined` tinyint(1) NOT NULL DEFAULT '0',"
-				"`kills_ircops` mediumint(9) NOT NULL,"
-				"`kills_server` mediumint(9) NOT NULL,"
-				"`opers` tinyint(4) NOT NULL,"
-				"`opers_max` tinyint(4) NOT NULL,"
-				"`opers_max_time` int(11) NOT NULL,"
+				"`kills_ircops` mediumint(9) NOT NULL,"   // remove this field ?
+				"`kills_server` mediumint(9) NOT NULL,"   // remove this field ?
+				"`opers` tinyint(4) NOT NULL,"            // remove this field ?
+				"`opers_max` tinyint(4) NOT NULL,"        // remove this field ?
+				"`opers_max_time` int(11) NOT NULL,"      // remove this field ?
 				"PRIMARY KEY (`id`),"
 				"UNIQUE KEY `name` (`name`),"
 				"KEY `linked_id` (`linked_id`)"
+				") ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+			this->RunQuery(query);
+		}
+		if (!this->HasTable(prefix + "chan"))
+		{
+			query = "CREATE TABLE `" + prefix + "chan` ("
+				"`chanid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,"
+				"`channel` varchar(255) NOT NULL,"
+				"`currentusers` int(15) NOT NULL DEFAULT 0,"
+				"`maxusers` int(15) NOT NULL DEFAULT 0,"
+				"`topic` varchar(255) DEFAULT NULL,"
+				"`topicauthor` varchar(255) DEFAULT NULL,"
+				"`topictime` datetime DEFAULT NULL,"
+				"`modes` varchar(512) DEFAULT NULL,"
+				"PRIMARY KEY (`chanid`),"
+				"UNIQUE KEY `channel`(`channel`),"
+				") ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+			this->RunQuery(query);
+		}
+		if (!this->HasTable(prefix + "user"))
+		{
+			query = "CREATE TABLE `" + prefix + "user` ("
+				"`nickid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,"
+				"`nick` varchar(255) NOT NULL DEFAULT '',"
+				"`realname` varchar(255) NOT NULL DEFAULT '',"
+				"`hostname` varchar(255) NOT NULL DEFAULT '',"
+				"`hiddenhostname` varchar(255) NOT NULL DEFAULT '',"
+				"`nickip` varchar(255) NOT NULL DEFAULT '',"
+				"`username` varchar(32) NOT NULL DEFAULT '',"
+				"`swhois` varchar(255) NOT NULL DEFAULT '',"
+				"`account` varchar(255) NOT NULL DEFAULT '',"
+				"`fingerprint` varchar(128) NOT NULL DEFAULT '',"
+				"`connecttime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',"
+				"`servid` int(11) UNSIGNED NOT NULL DEFAULT '0',"
+				"`server` varchar(255) NOT NULL DEFAULT '',"
+				"`away` enum('Y','N') NOT NULL DEFAULT 'N',"
+				"`awaymsg` varchar(255) NOT NULL DEFAULT '',"
+				"`ctcpversion` varchar(255) NOT NULL DEFAULT '',"
+				"`online` enum('Y','N') NOT NULL DEFAULT 'Y',"
+				"`countrycode` varchar(16) NOT NULL DEFAULT '',"
+				"`country` varchar(64) NOT NULL DEFAULT '',"
+				"PRIMARY KEY (`nickid`),"
+				"UNIQUE KEY `nick` (`nick`),"
+				"KEY `servid` (`servid`),"
 				") ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 			this->RunQuery(query);
 		}
@@ -164,6 +209,23 @@ class M_irc2sql : public Module
 	M_irc2sql(const Anope::string &modname, const Anope::string &creator) :
 		Module(modname, creator, EXTRA | THIRD), sql("", ""), sqlinterface(this)
 	{
+		quitting = false;
+	}
+
+	void OnShutdown()
+	{
+
+		// TODO: instead of looping through all users, channels and servers
+		//	 call a stored procedure that cleans the database.
+
+		// Test (remove it later)
+		for (Anope::map<Server *>::const_iterator it = Servers::ByName.begin(), it_end = Servers::ByName.end(); it != it_end; ++it)
+			this->OnServerQuit(it->second);
+
+		// send a blocking query
+		query = "SELECT 1";
+		SQL::Result r = this->sql->RunQuery(query);
+		quitting = true;
 	}
 
 	void OnReload(Configuration::Conf *config) anope_override
@@ -202,6 +264,9 @@ class M_irc2sql : public Module
 
 	void OnServerQuit(Server *server) anope_override
 	{
+		if (quitting)
+			return;
+
 		query = "INSERT DELAYED INTO `" + prefix + "servers` (name, online, split_time) "
 			"VALUES (@name@, '0', unix_timestamp()) "
 			"ON DUPLICATE KEY UPDATE name=VALUES(name), online=VALUES(online), split_time=VALUES(split_time)";
@@ -209,6 +274,18 @@ class M_irc2sql : public Module
 		this->RunQuery(query);
 	}
 
+	void OnUserConnect(User *u, bool &exempt)
+	{
+		/* TODO: - add more fields to the user table
+		 *	 - make a stored procedure
+		 */
+		query = "CALL " + prefix + "new_user(@nick@,@ident@,@vident@,@hostname@,@hiddenhostname@,"
+			"@nickip@,@realname@,@account@,@fingerprint@,@swhois@);";
+		query.SetValue("nick", u->nick());
+		query.SetValue("ident", u->ident);
+		query.SetValue("vident", u->vident);
+		this->RunQuery(query);
+	}
 };
 
 
